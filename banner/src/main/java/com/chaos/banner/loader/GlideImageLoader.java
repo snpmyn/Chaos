bf8b1a015e16192d17de92293af7392acddbc0db
snpmyn <snpmyn@126.com>
2021-03-05 15:59:13 +0800

    mtisse

--- banner/src/main/java/com/chaos/banner/loader/GlideImageLoader.java
+++ banner/src/main/java/com/chaos/banner/loader/GlideImageLoader.java
@@ -3,7 +3,7 @@ package com.chaos.banner.loader;
 import android.content.Context;
 import android.widget.ImageView;
 
-import com.chaos.utilone.glide.util.GlideUtils;
+import com.bumptech.glide.Glide;
 import com.youth.banner.loader.ImageLoader;
 
 /**
@@ -15,6 +15,6 @@ import com.youth.banner.loader.ImageLoader;
 public class GlideImageLoader extends ImageLoader {
     @Override
     public void displayImage(Context context, Object path, ImageView imageView) {
-        GlideUtils.loadByObject(context, path, imageView);
+        Glide.with(context).load(path).into(imageView);
     }
 }
---
+++ matisse/src/main/java/com/chaos/matisse/kit/MatisseKit.java
@@ -0,0 +1,106 @@
+package com.chaos.matisse.kit;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+
+import androidx.fragment.app.Fragment;
+
+import com.chaos.matisse.R;
+import com.chaos.matisse.engine.Glide4Engine;
+import com.zhihu.matisse.Matisse;
+import com.zhihu.matisse.MimeType;
+import com.zhihu.matisse.internal.entity.CaptureStrategy;
+
+import java.lang.ref.WeakReference;
+
+import timber.log.Timber;
+
+/**
+ * Created on 2018/12/6.
+ *
+ * @author 郑少鹏
+ * @desc Matisse 配套元件
+ */
+public class MatisseKit {
+    /**
+     * 请求码
+     */
+    public static final int REQUEST_CODE = 0x001;
+
+    /**
+     * 活动中 Matisse 准备
+     *
+     * @param activity  活动
+     * @param maxCount  最大数
+     * @param authority 权威（如 ${applicationId}.fileprovider）
+     * @param darkTheme 暗夜模式否（默 R.style.Matisse_Zhihu）
+     */
+    public static void matisseOperateInActivity(Activity activity, int maxCount, String authority, boolean darkTheme) {
+        WeakReference<Activity> weakReference = new WeakReference<>(activity);
+        Activity activityUse = weakReference.get();
+        Matisse.from(activityUse)
+                .choose(MimeType.ofImage(), false)
+                .countable(true)
+                .capture(true)
+                .captureStrategy(new CaptureStrategy(true, authority))
+                .maxSelectable(maxCount)
+                .gridExpectedSize(activityUse.getResources().getDimensionPixelSize(R.dimen.dp_130))
+                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                .thumbnailScale(0.85f)
+                // for glide-V3
+                // .imageEngine(new GlideEngine())
+                // for glide-V4
+                .imageEngine(new Glide4Engine())
+                .setOnSelectedListener((uriList, pathList) -> {
+                    // DO SOMETHING IMMEDIATELY HERE
+                    Timber.d("onSelected: pathList = %s", pathList);
+                })
+                .originalEnable(true)
+                .maxOriginalSize(10)
+                .autoHideToolbarOnSingleTap(true)
+                .setOnCheckedListener(isChecked -> {
+                    // DO SOMETHING IMMEDIATELY HERE
+                    Timber.d("onCheck: isChecked = %s", isChecked);
+                })
+                .theme(darkTheme ? R.style.Matisse_Dracula : R.style.Matisse_Zhihu)
+                .forResult(REQUEST_CODE);
+    }
+
+    /**
+     * 碎片中 Matisse 准备
+     *
+     * @param fragment  碎片
+     * @param maxCount  最大数
+     * @param authority 权威（如 ${applicationId}.fileprovider）
+     * @param darkTheme 暗夜模式否（默 R.style.Matisse_Zhihu）
+     */
+    public static void matisseOperateInFragment(Fragment fragment, int maxCount, String authority, boolean darkTheme) {
+        WeakReference<Fragment> weakReference = new WeakReference<>(fragment);
+        Matisse.from(weakReference.get())
+                .choose(MimeType.ofImage(), false)
+                .countable(true)
+                .capture(true)
+                .captureStrategy(new CaptureStrategy(true, authority))
+                .maxSelectable(maxCount)
+                .gridExpectedSize(weakReference.get().getResources().getDimensionPixelSize(R.dimen.dp_130))
+                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                .thumbnailScale(0.85f)
+                // for glide-V3
+                // .imageEngine(new GlideEngine())
+                // for glide-V4
+                .imageEngine(new Glide4Engine())
+                .setOnSelectedListener((uriList, pathList) -> {
+                    // DO SOMETHING IMMEDIATELY HERE
+                    Timber.d("onSelected: pathList = %s", pathList);
+                })
+                .originalEnable(true)
+                .maxOriginalSize(10)
+                .autoHideToolbarOnSingleTap(true)
+                .setOnCheckedListener(isChecked -> {
+                    // DO SOMETHING IMMEDIATELY HERE
+                    Timber.d("onCheck: isChecked = %s", isChecked);
+                })
+                .theme(darkTheme ? R.style.Matisse_Dracula : R.style.Matisse_Zhihu)
+                .forResult(REQUEST_CODE);
+    }
+}
