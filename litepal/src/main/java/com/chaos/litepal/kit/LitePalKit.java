e5389d18e739d1b18b3d0aac9f64a690d9471bdc
snpmyn <snpmyn@126.com>
2021-11-11 10:36:11 +0800

    优化本地数据库配套元件

--- litepal/src/main/java/com/chaos/litepal/kit/LitePalKit.java
+++ litepal/src/main/java/com/chaos/litepal/kit/LitePalKit.java
@@ -152,7 +152,7 @@ public class LitePalKit {
     }
 
     /**
-     * 选择并且排序查询
+     * 排序并且选择查询
      * <p>
      * asc 正序、desc 倒序。
      *
@@ -162,7 +162,267 @@ public class LitePalKit {
      * @param <T>        <T>
      * @return 结果集合
      */
-    public <T extends LitePalSupport> List<T> queryBySelectAndOrder(Class<T> modelClass, String column, String... columns) {
-        return LitePal.select(columns).order(column).find(modelClass);
+    public <T extends LitePalSupport> List<T> queryByOrderAndSelect(Class<T> modelClass, String column, String... columns) {
+        return LitePal.order(column).select(columns).find(modelClass);
+    }
+
+    /**
+     * 存在
+     *
+     * @param modelClass 模型类
+     * @param conditions 条件
+     * @param <T>        <T>
+     * @return 存在
+     */
+    public <T extends LitePalSupport> Boolean areExit(Class<T> modelClass, String... conditions) {
+        return LitePal.isExist(modelClass, conditions);
+    }
+
+    /**
+     * 计数
+     *
+     * @param tableName 表名
+     * @return 行数
+     */
+    public int count(String tableName) {
+        return LitePal.count(tableName);
+    }
+
+    /**
+     * 条件计数
+     *
+     * @param tableName  表名
+     * @param conditions 条件
+     * @return 行数
+     */
+    public int countByWhere(String tableName, String... conditions) {
+        return LitePal.where(conditions).count(tableName);
+    }
+
+    /**
+     * 计数
+     *
+     * @param modelClass 模型类
+     * @param <T>        <T>
+     * @return 行数
+     */
+    public <T extends LitePalSupport> int count(Class<T> modelClass) {
+        return LitePal.count(modelClass);
+    }
+
+    /**
+     * 条件计数
+     *
+     * @param modelClass 模型类
+     * @param conditions 条件
+     * @param <T>        <T>
+     * @return 行数
+     */
+    public <T extends LitePalSupport> int countByWhere(Class<T> modelClass, String... conditions) {
+        return LitePal.where(conditions).count(modelClass);
+    }
+
+    /**
+     * 求和
+     *
+     * @param tableName  表名
+     * @param columnName 列名
+     * @return 和
+     */
+    public int sum(String tableName, String columnName) {
+        return LitePal.sum(tableName, columnName, int.class);
+    }
+
+    /**
+     * 条件求和
+     *
+     * @param tableName  表名
+     * @param columnName 列名
+     * @param conditions 条件
+     * @return 和
+     */
+    public int sumByWhere(String tableName, String columnName, String... conditions) {
+        return LitePal.where(conditions).sum(tableName, columnName, Integer.TYPE);
+    }
+
+    /**
+     * 求和
+     *
+     * @param modelClass 模型类
+     * @param columnName 列名
+     * @param <T>        <T>
+     * @return 和
+     */
+    public <T extends LitePalSupport> int sum(Class<T> modelClass, String columnName) {
+        return LitePal.sum(modelClass, columnName, int.class);
+    }
+
+    /**
+     * 条件求和
+     *
+     * @param modelClass 模型类
+     * @param columnName 列名
+     * @param conditions 条件
+     * @param <T>        <T>
+     * @return 和
+     */
+    public <T extends LitePalSupport> int sumByWhere(Class<T> modelClass, String columnName, String... conditions) {
+        return LitePal.where(conditions).sum(modelClass, columnName, Integer.TYPE);
+    }
+
+    /**
+     * 平均
+     * <p>
+     * 具有运算能力列可求平均值。
+     * 字符串类型列无法求平均值，返 0。
+     *
+     * @param tableName  表名
+     * @param columnName 列名
+     * @return 均值
+     */
+    public Double average(String tableName, String columnName) {
+        return LitePal.average(tableName, columnName);
+    }
+
+    /**
+     * 条件平均
+     * <p>
+     * 具有运算能力列可求平均值。
+     * 字符串类型列无法求平均值，返 0。
+     *
+     * @param tableName  表名
+     * @param columnName 列名
+     * @param conditions 条件
+     * @return 均值
+     */
+    public Double averageByWhere(String tableName, String columnName, String... conditions) {
+        return LitePal.where(conditions).average(tableName, columnName);
+    }
+
+    /**
+     * 平均
+     * <p>
+     * 具有运算能力列可求平均值。
+     * 字符串类型列无法求平均值，返 0。
+     *
+     * @param modelClass 模型类
+     * @param columnName 列名
+     * @param <T>        <T>
+     * @return 均值
+     */
+    public <T extends LitePalSupport> Double average(Class<T> modelClass, String columnName) {
+        return LitePal.average(modelClass, columnName);
+    }
+
+    /**
+     * 条件平均
+     * <p>
+     * 具有运算能力列可求平均值。
+     * 字符串类型列无法求平均值，返 0。
+     *
+     * @param modelClass 模型类
+     * @param columnName 列名
+     * @param conditions 条件
+     * @param <T>        <T>
+     * @return 均值
+     */
+    public <T extends LitePalSupport> Double averageByWhere(Class<T> modelClass, String columnName, String... conditions) {
+        return LitePal.where(conditions).average(modelClass, columnName);
+    }
+
+    /**
+     * 求最大值
+     *
+     * @param tableName  表名
+     * @param columnName 列名
+     * @return 最大值
+     */
+    public int max(String tableName, String columnName) {
+        return LitePal.max(tableName, columnName, int.class);
+    }
+
+    /**
+     * 条件求最大值
+     *
+     * @param tableName  表名
+     * @param columnName 列名
+     * @param conditions 条件
+     * @return 最大值
+     */
+    public int maxByWhere(String tableName, String columnName, String... conditions) {
+        return LitePal.where(conditions).max(tableName, columnName, Integer.TYPE);
+    }
+
+    /**
+     * 求最大值
+     *
+     * @param modelClass 模型类
+     * @param columnName 列名
+     * @param <T>        <T>
+     * @return 最大值
+     */
+    public <T extends LitePalSupport> int max(Class<T> modelClass, String columnName) {
+        return LitePal.max(modelClass, columnName, int.class);
+    }
+
+    /**
+     * 条件求最大值
+     *
+     * @param modelClass 模型类
+     * @param columnName 列名
+     * @param conditions 条件
+     * @param <T>        <T>
+     * @return 最大值
+     */
+    public <T extends LitePalSupport> int maxByWhere(Class<T> modelClass, String columnName, String... conditions) {
+        return LitePal.where(conditions).max(modelClass, columnName, int.class);
+    }
+
+    /**
+     * 求最小值
+     *
+     * @param tableName  表名
+     * @param columnName 列名
+     * @return 最小值
+     */
+    public int min(String tableName, String columnName) {
+        return LitePal.min(tableName, columnName, int.class);
+    }
+
+    /**
+     * 条件求最小值
+     *
+     * @param tableName  表名
+     * @param columnName 列名
+     * @param conditions 条件
+     * @return 最小值
+     */
+    public int minByWhere(String tableName, String columnName, String... conditions) {
+        return LitePal.where(conditions).min(tableName, columnName, Integer.TYPE);
+    }
+
+    /**
+     * 求最小值
+     *
+     * @param modelClass 模型类
+     * @param columnName 列名
+     * @param <T>        <T>
+     * @return 最小值
+     */
+    public <T extends LitePalSupport> int min(Class<T> modelClass, String columnName) {
+        return LitePal.min(modelClass, columnName, int.class);
+    }
+
+    /**
+     * 条件求最小值
+     *
+     * @param modelClass 模型类
+     * @param columnName 列名
+     * @param conditions 条件
+     * @param <T>        <T>
+     * @return 最小值
+     */
+    public <T extends LitePalSupport> int minByWhere(Class<T> modelClass, String columnName, String... conditions) {
+        return LitePal.where(conditions).min(modelClass, columnName, int.class);
     }
 }
